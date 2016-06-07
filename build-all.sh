32fde76a47bd5d214c536d05fe33b52461bd2137
David Antliff <david.antliff@imgtec.com>
2016-06-07 14:40:00 +1200

    Fix for non-TTY execution.
    Signed-off-by: David Antliff <david.antliff@imgtec.com>

--- build-all.sh
+++ build-all.sh
@@ -4,7 +4,13 @@
 set -o errexit
 set -o nounset
 
+if [ -t 1 ]; then
+  TERMINAL_OPT=-t
+else
+  TERMINAL_OPT=
+fi
+
 #docker rmi -f registry-hh.flowcloud.systems:5000/flowm2m/awalwm2m-deb:latest
 #docker pull registry-hh.flowcloud.systems:5000/flowm2m/awalwm2m-deb:latest
 docker build -t awalwm2m-deb -f Dockerfile .
-docker run -e GITHUB_COMMIT=$GITHUB_COMMIT -e PACKAGE_VERSION=$PACKAGE_VERSION -v $(pwd):/build -it awalwm2m-deb /build/build-deb.sh
+docker run -e GITHUB_COMMIT=$GITHUB_COMMIT -e PACKAGE_VERSION=$PACKAGE_VERSION -v $(pwd):/build -i $TERMINAL_OPT awalwm2m-deb /build/build-deb.sh
